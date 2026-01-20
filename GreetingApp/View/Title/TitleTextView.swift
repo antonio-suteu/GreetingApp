8069673c7941cc0f04c2dcaa9a0d92cdbf21455f
Antonio Suteu <suteu.antonio@gmail.com>
2026-01-20 21:42:20 +0100

    Added text rotation animation in TitleTextView.swift

--- GreetingApp/View/Title/TitleTextView.swift
+++ GreetingApp/View/Title/TitleTextView.swift
@@ -5,6 +5,8 @@ struct TitleTextView: View {
     // This binding allows to change the state of the parent view that uses this
     @Binding var subtitle: LocalizedStringKey
 
+    @State private var rotationDegrees: Double = 0
+
     /// A collection of possible subtitles that can be randomly selected.
     let subtitles: [LocalizedStringKey] = [
         "— Exploring iOS Programming —",
@@ -30,6 +32,11 @@ struct TitleTextView: View {
             .font(font)
             .fontDesign(.monospaced)
             .onTapGesture {
+                // Animate the rotation
+                withAnimation(.easeInOut(duration: 0.5)) {
+                    rotationDegrees += 360
+                }
+
                 // Select a random subtitle that's different from the current one
                 var randomPick: LocalizedStringKey = subtitles.randomElement() ?? subtitle
                 while randomPick == subtitle {
@@ -38,8 +45,9 @@ struct TitleTextView: View {
 
                 subtitle = randomPick
             }
-            .padding()
+            .rotation3DEffect(.degrees(rotationDegrees), axis: (x: 0, y: 1, z: 0))
             .popoverTip(greetingsTip)
+            .padding()
     }
 }
 
