0b9438882231e80277a9383bf8e7071d8af32438
Antonio Suteu <suteu.antonio@gmail.com>
2026-02-01 15:32:28 +0100

    Added align modifier

---
+++ GreetingApp/View/Components/AlignModifier.swift
@@ -0,0 +1,18 @@
+//
+//  AlignModifier.swift
+//  GreetingApp
+//
+//  Created by Antonio Suteu on 01/02/26.
+//
+
+import SwiftUI
+
+struct AlignModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlignModifier()
+}
